27fecd826690d27a4dfbe3e92cf12df288005f89
sirusdynamo <sirusdynamo@gmail.com>
2019-01-29 09:12:16 +0100

    Jobs app up amd running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField( blank=True, null=True)
+    body_text = models.TextField()
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
