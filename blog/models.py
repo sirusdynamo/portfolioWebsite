668d981ac43ff0f33e5f78d66d517ed3d9bc6777
sirusdynamo <sirusdynamo@gmail.com>
2019-01-31 13:07:10 +0100

    Added resume pdf and image pdf, created detail view

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,16 @@ class Blog(models.Model):
     pub_date = models.DateTimeField( blank=True, null=True)
     body_text = models.TextField()
     image = models.ImageField(upload_to='images/',blank=True, null=True)
+
+
+    def __str__(self):
+        return self.blog_title
+
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%B %d %Y')
+        
+
+    def summary(self):
+
+        return ''.join(self.body_text.split('.')[0:2])
